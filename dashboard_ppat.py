7f8d0ccc78a3c05503e9d91f1c07743e978185e5
atrbpnkanwilkalsel01-cmyk <atrbpnkanwilkalsel01@gmail.com>
2026-03-20 16:56:25 +0800

    Update dashboard_ppat.py
    Signed-off-by: atrbpnkanwilkalsel01-cmyk <atrbpnkanwilkalsel01@gmail.com>

--- dashboard_ppat.py
+++ dashboard_ppat.py
@@ -83,8 +83,6 @@ try:
     
     fig = px.bar(counts, x='Jumlah', y='PPAT', orientation='h', text='Jumlah')
     fig.update_layout(height=max(400, len(counts)*30), yaxis={'categoryorder':'total ascending'}, xaxis_title="", yaxis_title="")
-    
-    st.plotly_chart(fig, use_container_width=True
 
 @st.cache_data(ttl=60)
 def load():
